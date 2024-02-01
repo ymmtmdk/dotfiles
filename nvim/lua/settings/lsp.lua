8e6d50ff9d7c27dc4624bc40d4df69709d6eeb41
ymmtmdk <ymmtmdk@gmail.com>
2024-02-01 19:13:14 +0900

    fix volar takeover mode

--- nvim/lua/settings/lsp.lua
+++ nvim/lua/settings/lsp.lua
@@ -8,20 +8,26 @@ require("neoconf").setup({
 require("mason-lspconfig").setup {
   ensure_installed = { "lua_ls", "volar" },
 }
-require('mason-lspconfig').setup_handlers({ function(server)
-  local opt = {
-    -- -- Function executed when the LSP server startup
-    -- on_attach = function(client, bufnr)
-    --   local opts = { noremap=true, silent=true }
-    --   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    --   vim.cmd 'autocmd BufWritePre * lua vim.lsp.buf.formatting_sync(nil, 1000)'
-    -- end,
-    capabilities = require('cmp_nvim_lsp').default_capabilities(
-    vim.lsp.protocol.make_client_capabilities()
-    )
-  }
-  require('lspconfig')[server].setup(opt)
-end })
+
+require('mason-lspconfig').setup_handlers({
+  function(server)
+    if require("neoconf").get(server .. ".disable") then
+      return
+    end
+    local opt = {
+      -- -- Function executed when the LSP server startup
+      -- on_attach = function(client, bufnr)
+      --   local opts = { noremap=true, silent=true }
+      --   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+      --   vim.cmd 'autocmd BufWritePre * lua vim.lsp.buf.formatting_sync(nil, 1000)'
+      -- end,
+      capabilities = require('cmp_nvim_lsp').default_capabilities(
+      vim.lsp.protocol.make_client_capabilities()
+      )
+    }
+    require('lspconfig')[server].setup(opt)
+  end
+})
 
 require'lspconfig'.lua_ls.setup{
   settings = {
@@ -32,6 +38,10 @@ require'lspconfig'.lua_ls.setup{
       },
     },
   },
+}
+
+require'lspconfig'.volar.setup{
+  filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json'}
 }
 
 -- 2. completion (hrsh7th/nvim-cmp)
--- nvim/lua/settings/plugin.lua
+++ nvim/lua/settings/plugin.lua
@@ -14,6 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   {
     'williamboman/mason.nvim',
+    lazy = true,
     cmd = {
       "Mason",
       "MasonInstall",
