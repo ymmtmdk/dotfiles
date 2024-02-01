b1531ffb5a793b17b4f3f18750bd48e10f96883e
ymmtmdk <ymmtmdk@gmail.com>
2024-02-01 22:06:05 +0900

    NC

---
+++ nvim/lua/plugins/tokyonight.lua
@@ -0,0 +1,16 @@
+return {
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {},
+  config = function()
+    require("tokyonight").setup({
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    })
+    vim.cmd [[colorscheme tokyonight-moon]]
+  end
+}
