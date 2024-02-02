e79ac935305c792e74f3d158352352c0f5c83e75
ymmtmdk <ymmtmdk@gmail.com>
2024-02-02 18:45:56 +0900

    NC

--- nvim/lua/plugins/auto-save.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "ymmtmdk/auto-save.nvim",
-  lazy = false,
-  config = function()
-    require("auto-save").setup {
-      debounce_delay = 500,
-    }
-  end
-}
---
+++ nvim/lua/plugins/vim-auto-save.lua
@@ -0,0 +1,11 @@
+return {
+  'vim-scripts/vim-auto-save',
+  lazy = false,
+  config = function()
+    vim.g.auto_save = 1
+    -- vim.g.auto_save_silent = 1
+    vim.g.auto_save_no_updatetime = 1  --  " do not change the 'updatetime' option
+    vim.opt.updatetime = 500
+    vim.g.auto_save_in_insert_mode = 0 --  " do not save while in insert mode
+  end
+}
