08723c134e991486f104e50878d92b0c1b726e14
ymmtmdk <ymmtmdk@gmail.com>
2024-02-02 19:00:56 +0900

    NC

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,6 +1,6 @@
 return {
-  lazy = false,
   'nvim-lualine/lualine.nvim',
+  event = { "BufReadPre", "BufNewFile" },
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
     require('lualine').setup {
